e9a634c7499726577d59ab818df9fad09c2833dc
TsuruharaKota <m1830@s.akashi.ac.jp>
2019-06-29 16:38:45 +0900

    とりあえず上げとく

---
+++ sensor/gy521.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<pigpiod_if2.h>
+#include<chrono>
+
+using std::cout;
+using std::endl;
+
+constexpr double resolution = 131.0;
+
+typedef enum{
+    MPU_WHO_AM_I 0x75;
+    MPU_SET 0x6B;
+    MPU_ADDRESS 0x68;
+    GYRO_ZOUT_H 0x47;
+    GYRO_ZOUT_L 0x48;
+}RegisterMap;
+
+int main(){
+    //setting i2c
+    //get time_now
+    //time_diff = time_now - time_prev;
+    //get the each data
+    //time_prev = time_now
+    //calculate the sensor angle(degree);
+    //display the result
+}
+
