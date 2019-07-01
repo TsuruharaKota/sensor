0374243eee7674096ab45f81596d2032d78edebc
TsuruharaKota <m1830@s.akashi.ac.jp>
2019-07-01 15:12:13 +0000

    しゅーせー

--- sensor/gy521.cpp
+++ sensor/gy521.cpp
@@ -4,12 +4,11 @@
 
 using std::cout;
 using std::endl;
-using std::chrono;
 
-constexpr double resolution = 131.0;
+//constexpr double resolution = 131.0;
 int pi;
 double degree = 0;
-typedef enum{
+/*typedef enum{
     MPU_WHO_AM_I = 0x75;
     MPU_SET = 0x6B;
     MPU_SIGNAL = 0x3B;
@@ -17,22 +16,22 @@ typedef enum{
     GYRO_ZOUT_H = 0x47;
     GYRO_ZOUT_L = 0x48;
 }RegisterMap;
-
+*/
 int main(){
     int pi = pigpio_start(0, 0);
-    RegisterMap set = MPU_SET;
-    RegisterMap add = MPU_ADDRESS;
+  //  RegisterMap set = MPU_SET;
+  //  RegisterMap add = MPU_ADDRESS;
     while(1){
-        int handle_data = i2cOpen(pi, device, RegisterMap MPU_ADDRESS, 0);
-        i2c_write_byte(pi, handle_data, &MPU_SET, 0x00);
-        int16_t gzRaw = i2c_read_byte_data(pi, handle_data, 0X47) << 8 | i2c_read_byte_data(pi, handle_data, 0x48);
+        auto time_start = std::chrono::system_clock::now();
+        int handle_datum = i2cOpen(pi, device, 0x68, 0);
+        i2c_write_byte(pi, handle_datum, 0x6B, 0x00);
+        int16_t gzRaw = i2c_read_byte_data(pi, handle_datum, 0X47) << 8 | i2c_read_byte_data(pi, handle_datum, 0x48);
         double gyro_z = gzRaw / 131.0;
-        auto time_end = system_clock::now();
-        auto time_diff = time_end - time_start;
-        degree += gyro_z * time_diff
+        auto time_end = std::chrono::system_clock::now();
+        double time_diff = std::chrono::duration_cast<std::chrono::milliseconds>(time_end-time_start).count();
+        degree += gyro_z * time_diff;
             cout << degree << endl;
-        auto time_start = system_clock::now();
     }
-    i2c_close(pi, handle_data);
+    i2c_close(pi, handle_datum);
 }
 
